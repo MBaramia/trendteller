7db75edfe5d3c976703e9252e2b9b51cf2e27dad
Rushen <66794838+15rufernan@users.noreply.github.com>
2024-02-28 01:07:13 +0000

    Added skeleton for auth

---
+++ client/src/Auth.js
@@ -0,0 +1,23 @@
+function setUpAuth() {
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: "username@email.com",
+      password: "password123",
+    })
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error("Error logging in");
+    }
+    return response.json();
+  }).then(data => {
+    console.log(data);
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+export default setUpAuth;
